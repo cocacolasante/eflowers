93a2de42c4ec15c88ec27c285669febae6fe021b
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2024-03-28 18:51:21 -0400

    init commit

---
+++ components/Navbar.js
@@ -0,0 +1,19 @@
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Services</li>
+                <li>Contact Us</li>
+                <li>Order Now</li>
+            </ul>
+
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
